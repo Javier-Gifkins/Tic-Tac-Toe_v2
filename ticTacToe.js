22ee4df45415d2d0742a23b591a02f875cca8b71
jav <javier-gifkins@live.nmit.ac.nz>
2025-05-10 22:43:05 +1200

    Added multiple comments

--- ticTacToe.js
+++ ticTacToe.js
@@ -5,9 +5,7 @@ const readlineSync = require('readline-sync');
 //NOTE - Step 1: Board Representation
 
 // a 3x3 array is used to represent the game board. The board array is a 2D array, meaning it's an array that contains other arrays as elements. Each inner array represents a horizontal row on the board.
-
 //  Each cell will first be empty, and updated with 'X' for the player and 'O' for the AI.
-
 let board = [
     [' ', ' ',' '], // row 0
     [' ', ' ',' '], // row 1
@@ -18,9 +16,12 @@ let board = [
 
 function printBoard() {
     console.log("Board:");
+    // this loop will run multiple times, once for each row
     for (let row of board) {
+        // log the updated row and seperate each item in the 2d array with "|" 
         console.log(row.join(" | "));
-        if (row != board[board.length - 1]) {
+        // if row is not last row, print "--+---+--" after
+        if (row != board[board.length - 1]) { 
             console.log("--+---+--");
         }
     } 
